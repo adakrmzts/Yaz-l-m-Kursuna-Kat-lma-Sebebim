2bd0eb334840f38f4aab36d8fcea3f0a0cfdb619
Alpay <Alpay@ALPAY>
2024-12-23 21:06:50 +0300

    Add project files.

---
+++ Sebebim/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Merhaba, ben Ada.");
+Console.WriteLine("Köyde yaşadığım ve sosyalleşemediğim için, bir yandan da yenilikleri takip etmek ve kendimi dinç tutmak adına başladığı bir kurstayım.Amacım iş dalımı otomasyon mühendisliğine kaydırmak, kaliteli bir çevre edinmek ve kendine yatırım yapan insanlar ile network oluşturmak. :)");
